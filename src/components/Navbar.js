7db6ce185c4172cbd33e11372fb22c3e893d08d0
Carlos Nunes <carlostaekwon@gmail.com>
2022-11-06 20:44:49 -0300

    2 menus

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,19 @@
 import {Link} from 'react-router-dom'
 import styles from './Navbar.module.css'
-import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
-import Button from 'react-bootstrap/Button';
+import {Button, Container, Offcanvas} from 'react-bootstrap'  
+import {useState} from 'react'  
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 
 export default function ColorSchemesExample() {
+  const [show, setShow] = useState(false);  
+  const closeSidebar = () => setShow(false);  
+  const showSidebar = () => setShow(true);  
   return (
-    
-      <Navbar className={styles.navBar} expand="lg">
+    <>
+      <Navbar className={styles.navBarDesk} expand="lg">
         <Container>
           <Navbar.Brand>            
             <Link className={styles.navBrand} to="/">Navbar</Link>
@@ -30,6 +33,37 @@ export default function ColorSchemesExample() {
           </Navbar.Collapse>
         </Container>
       </Navbar>
+
+      <Navbar className={styles.navBarMob} expand="lg">
+        <Container>
+
+          <Navbar.Brand>            
+            <Link className={styles.navBrand} to="/">Navbar</Link>
+          </Navbar.Brand>
+
+          <Navbar.Toggle onClick={showSidebar} className="ms-auto"/>
+          <Container>  
+
+            <Offcanvas show={show} onHide={closeSidebar}>  
+
+              <Offcanvas.Header closeButton>  
+                <Offcanvas.Title>Menu</Offcanvas.Title>  
+              </Offcanvas.Header>  
+              
+              <Offcanvas.Body>  
+                  <Link className={styles.sideLink} to="/">Home</Link>
+                  <Link className={styles.sideLink} to="/quem">Quem sou</Link>
+              </Offcanvas.Body>  
+
+            </Offcanvas> 
+
+          
+        </Container>
+        </Container>
+      </Navbar>
+
+    </>
+      
 
 
   )
