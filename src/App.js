bdd00bb9d2cd2566070311c5a857ddb774ae128c
Carlos Nunes <carlostaekwon@gmail.com>
2023-01-12 17:18:41 -0300

    correção router

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ export default function App() {
   return (
     <div className="App">
     
-      <Router basename="/portfolio">     
+      <Router>     
         <Navbar/>
         <Routes>
     
